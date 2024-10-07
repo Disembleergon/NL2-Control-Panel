746fb1cc51578eac82a201955ab8e138c1f9d51a
Tom Pekrul <tom.pekrul1@gmail.com>
2024-10-07 15:12:14 +0200

    add basic server code/structure

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,36 @@
+const PORT: u16 = 3000;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: pretty banner with short explanation
+    // TODO: show external ip address
+    println!("Server listening on localhost:{PORT}");
+
+    rouille::start_server(format!("127.0.0.1:{PORT}"), move |request| {
+        {
+            let response = rouille::match_assets(request, "static");
+            if response.is_success() {
+                return response;
+            }
+        }
+
+        rouille::router!(request,
+
+            (GET) (/) => {
+                rouille::Response::redirect_303("/index.html")
+            },
+
+            (GET) (/panel) => {
+                rouille::Response::redirect_303("/panel.html")
+            },
+
+            (POST) (/) => {
+                // TODO: press keys according to specified action
+                rouille::Response::text("key tap")
+            },
+
+            _ => {
+                rouille::Response::redirect_301("/index.html")
+            }
+        )
+    });
 }
